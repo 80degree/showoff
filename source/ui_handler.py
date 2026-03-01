9dde0312c8537ee41bb4a47f9b54ace5b6e4ffa4
worthyworm <99085361+worthyworm@users.noreply.github.com>
2026-03-01 19:37:48 +0300

    Add language saving

--- source/ui_handler.py
+++ source/ui_handler.py
@@ -1,28 +1,29 @@
 import os
 import json
-import sys
 
 from __init__ import INFO
 
 while True:
     try:
-        lang = int(input("[1] - English\n[2] - Русский\n>>>"))
-        if lang == 1:
-            lang = 'en'
-            break
-        elif lang == 2:
-            lang = 'ru'
-            break
-        else:
-            print("Out of range.")
-
-    except ValueError:
-        print('Invalid choice')
-
-    except Exception as e:
-        print(f'ERROR: {e}')
-        input('Press Enter to exit...')
-        sys.exit(1)
+        with open('preferences.json', 'r', encoding='utf-8') as f:
+            preferences = json.load(f)
+            lang = preferences['lang']
+        break
+    except FileNotFoundError:
+        with open('preferences.json', 'w', encoding='utf-8') as f:
+            lang = int(input("[1] - English\n[2] - Русский\n>>>"))
+            if lang == 1:
+                lang = 'en'
+                with open('preferences.json', 'w', encoding='utf-8') as f:
+                    json.dump({"lang": lang}, f, ensure_ascii=False, indent=4)
+                break
+            elif lang == 2:
+                lang = 'ru'
+                with open('preferences.json', 'w', encoding='utf-8') as f:
+                    json.dump({"lang": lang}, f, ensure_ascii=False, indent=4)
+                break
+            else:
+                print("Out of range.")
 
 
 with open(f'locales/lang_{lang}.json', 'r', encoding='utf-8') as f:
